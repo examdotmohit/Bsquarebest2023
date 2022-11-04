480fd39398cd7dbbefa30dd600973b9e900d9d6c
mohit bajaj <examdotcom@gmail.com>
2022-11-04 12:33:14 +0530

    asdf

--- Signup.aspx.cs
+++ Signup.aspx.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
 
 public partial class Signup : System.Web.UI.Page
 {
     public maincontent maincontent = new maincontent();
+    private SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
+
     protected void Page_Load(object sender, EventArgs e)
     {
+    }
 
+    protected void photosubmit_ServerClick(object sender, EventArgs e)
+    {
+    }
+
+    protected void mobcheck_ServerClick(object sender, EventArgs e)
+    {
+        con.Open();
+        string cmdstr81 = "select count(*) from students where mobno='" + senderMobile.Text + "'";
+        SqlCommand checkuser81 = new SqlCommand(cmdstr81, con);
+        int temp81 = Convert.ToInt32(checkuser81.ExecuteScalar().ToString());
+        con.Close();
+        if (temp81 >= 1)
+        {
+            mobnopanel.Visible = false;
+            mainformpanel.Visible = true;
+            finalpanel.Visible = false;
+        }
+        else
+        {
+            mobnopanel.Visible = true;
+            mainformpanel.Visible = false;
+            finalpanel.Visible = false;
+        }
     }
 }
---
+++ upload.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Web;
+
+public partial class upload : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string logfileaddress = HttpContext.Current.Server.MapPath("/") + @"log.txt";
+        maincontent maincontent1 = new maincontent();
+        string path = logfileaddress;
+        if (!File.Exists(path))
+        {
+        }
+        else
+        {
+            if (Request.HttpMethod.ToUpper() == "POST")
+            {
+                Response.Clear();
+                string json = new StreamReader(Request.InputStream).ReadToEnd();
+                string text1 = "Call " + maincontent1.datetimesend() + " " + json + "\n";
+                File.AppendAllText(path, text1);
+                Response.Write("working");
+            }
+        }
+    }
+}
